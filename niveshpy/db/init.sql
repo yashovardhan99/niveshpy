fc6c35f1ec96cd8074699a7f75ce0e2f15d1e0d0
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2025-10-12 17:06:43 +0530

    Move types/categories to enums in sql query

--- niveshpy/db/init.sql
+++ niveshpy/db/init.sql
@@ -4,15 +4,24 @@ CREATE TABLE IF NOT EXISTS accounts (
     name VARCHAR NOT NULL,
     institution VARCHAR NOT NULL
 );
+CREATE TYPE IF NOT EXISTS SecurityType AS ENUM ('stock', 'bond', 'etf', 'mutual_fund', 'other');
+CREATE TYPE IF NOT EXISTS SecurityCategory AS ENUM (
+    'equity',
+    'debt',
+    'commodity',
+    'real_estate',
+    'other'
+);
 CREATE TABLE IF NOT EXISTS securities (
     key VARCHAR PRIMARY KEY,
     name VARCHAR NOT NULL,
-    type VARCHAR NOT NULL,
-    category VARCHAR NOT NULL
+    type SecurityType NOT NULL,
+    category SecurityCategory NOT NULL
 );
+CREATE TYPE IF NOT EXISTS TransactionType AS ENUM ('purchase', 'sale');
 CREATE TABLE IF NOT EXISTS transactions (
     transaction_date DATE,
-    type TEXT,
+    type TransactionType NOT NULL,
     description TEXT,
     amount DECIMAL(24, 2),
     units DECIMAL(24, 3),
