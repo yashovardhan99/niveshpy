08946ebbf03a2e746183cd264214bcf6572ddc1f
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2026-01-03 16:46:05 +0530

    Add tests for parsing service

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,12 @@ def engine():
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
 
+    from niveshpy.models.account import Account  # noqa: F401
+    from niveshpy.models.price import Price  # noqa: F401
+    from niveshpy.models.security import Security  # noqa: F401
+    from niveshpy.models.transaction import Transaction  # noqa: F401
+
+    # Import all models to ensure they are registered with SQLModel
     SQLModel.metadata.create_all(engine)
     return engine
 
