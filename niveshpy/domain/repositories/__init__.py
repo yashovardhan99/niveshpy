43b3a0691d641d9c1c940ac2f83c15742f8201b9
Yashovardhan Dhanania <yashovardhandhanania@gmail.com>
2026-04-10 00:07:57 +0530

    Add price repository protocol

--- niveshpy/domain/repositories/__init__.py
+++ niveshpy/domain/repositories/__init__.py
@@ -1,7 +1,13 @@
 """Package for abstract repository interfaces in the domain layer."""
 
 from .account_repository import AccountRepository
+from .price_repository import PriceRepository
 from .security_repository import SecurityRepository
 from .transaction_repository import TransactionRepository
 
-__all__ = ["AccountRepository", "SecurityRepository", "TransactionRepository"]
+__all__ = [
+    "AccountRepository",
+    "SecurityRepository",
+    "TransactionRepository",
+    "PriceRepository",
+]
